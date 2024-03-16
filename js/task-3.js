4ae8d6fcae5857eba4de93be1da6c33572491cbc
Verona0609 <dikaya.nika15@gmail.com>
2024-03-16 18:12:20 +0100

    hw

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,9 @@
  function getElementWidth(content, padding, border){
-    const Npx=content+(padding*2) + (border*2);
-    return Npx;
+    let contentWidth = parseFloat(content);
+    let paddingWidth = parseFloat(padding);
+    let borderWidth = parseFloat(border);
+     const totalWidth=contentWidth + paddingWidth * 2 + borderWidth * 2;
+     return totalWidth;
     }
     console.log(getElementWidth("50px", "8px", "4px")); // 74
     console.log(getElementWidth("60px", "12px", "8.5px")); // 101
